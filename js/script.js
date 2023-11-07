0bf07038782b03bcc43be5a08a0bdf6c003ab239
Talles Gomes <talles311@outlook.com>
2023-11-07 10:47:32 +0000

    atualizado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log(`teste`);
+
+// ativar links do menu 
+
+const links = document.querySelectorAll(".header-menu li a");
+// console.log(link); ---> aqui podemos usar o console.log apenas para verificar se está tudo correndo bem, se funcionam! 
+               
+// nome da função e suas variaveis:  ativarLink 
+function ativarLink(link) {
+       const url = location.href;  
+       const href = link.href; 
+       if (url.includes(href)) {
+            link.classList.add("ativo");
+     }
+    }
+
+// Ativar uma função para cada um dos links  
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search); 
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);  
+    elemento.checked = true;
+    console.log(elemento);
+  }
+ 
+ parametros.forEach(ativarProduto);
